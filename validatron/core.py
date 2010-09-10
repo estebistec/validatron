e09aa319862c40dbfd3bbb5f72369a771ab5b60c
Steven Cummings <steven@cummingsbrothers.net>
2010-09-09 23:58:54 -0500

    Updated structural validation method (still not entirely liking how it looks...)

--- validatron/core.py
+++ validatron/core.py
@@ -1,6 +1,16 @@
+__all__ = ('ValidationError', 'string', 'floating_point', 'integer', 'decimal', 'boolean')
+
+
+from decimal import Decimal
+
+
 class ValidationError(ValueError):
     def __init__(self, problems):
         self.problems = problems
+
+
+# TODO datetime
+
 
 def string(optional=False, min_length=1, max_length=None, pattern=None):
     def is_valid(value):
@@ -18,10 +28,11 @@ def string(optional=False, min_length=1, max_length=None, pattern=None):
                 return "missing"
     return is_valid
 
+
 def integer(optional=False, min_value=None, max_value=None):
     def is_valid(value):
         if value is not None:
-            if not isinstance(value, int, long):
+            if not isinstance(value, (int, long)):
                 return "not a whole number"
             if min_value is not None and value < min_value:
                 return "too small"
@@ -33,5 +44,46 @@ def integer(optional=False, min_value=None, max_value=None):
                 return "missing"
     return is_valid
 
-# TODO float
-# TODO datetime
+
+def floating_point(optional=False, min_value=None, max_value=None):
+    def is_valid(value):
+        if value is not None:
+            if not isinstance(value, float):
+                return "not a floating-point number"
+            if min_value is not None and value < min_value:
+                return "too small"
+            if max_value is not None and value > max_value:
+                return "too large"
+            return None
+        else:
+            if not optional:
+                return "missing"
+    return is_valid
+
+
+def decimal(optional=False, min_value=None, max_value=None):
+    def is_valid(value):
+        if value is not None:
+            if not isinstance(value, Decimal):
+                return "not a decimal number"
+            if min_value is not None and value < min_value:
+                return "too small"
+            if max_value is not None and value > max_value:
+                return "too large"
+            return None
+        else:
+            if not optional:
+                return "missing"
+    return is_valid
+
+
+def boolean(optional=False):
+    def is_valid(value):
+        if value is not None:
+            if not isinstance(value, bool):
+                return "not a boolean"
+            return None
+        else:
+            if not optional:
+                return "missing"
+    return is_valid
--- validatron/validation.py
+++ validatron/validation.py
@@ -1,11 +1,30 @@
+__all__ = ('validate',)
+
 def validate(value, validator):
     """
-    >>> from core import string, integer
-    >>> v = {"a": string(optional=True), "b": string()}
-    >>> validate({"a": "aString", "b": "anotherString"}, v)
-    >>> validate({"b": "anotherString"}, v)
-    >>> validate({}, v)
+    >>> from core import string, integer,boolean
+    >>>
+    >>> v_dict = {"a": string(optional=True), "b": string(), "c": integer()}
+    >>> validate({"a": "aString", "b": "anotherString", "c": 1}, v_dict)
+    >>> validate({"b": "anotherString", "c": 1}, v_dict)
+    >>> validate({"c": 1}, v_dict)
     {'b': 'missing'}
+    >>> validate({"b": "anotherString"}, v_dict)
+    {'c': 'missing'}
+    >>> validate({"b": "anotherString", "c": "notAnInt"}, v_dict)
+    {'c': 'not a whole number'}
+    >>>
+    >>> v_list = [boolean()]
+    >>> validate([True, True, False], v_list)
+    >>> validate([True, "True", False], v_list)
+    {1: 'not a boolean'}
+    >>>
+    >>> v_tuple = (string(), integer())
+    >>> validate(("a", 1), v_tuple)
+    >>> validate(("a", 1.0), v_tuple)
+    {1: 'not a whole number'}
+    >>> validate(("a",), v_tuple)
+    {'__all__': 'unexpected tuple length: expected 2, got 1'}
     """
     if isinstance(validator, dict):
         if not isinstance(value, dict):
@@ -14,8 +33,9 @@ def validate(value, validator):
     elif isinstance(validator, list):
         if not isinstance(validator, list):
             return {'__all__': 'Wrong type. Expected list, got {type}'.format(type=type(value))}
-        return _validate_sequence(value, validator)
-    # TODO tuple
+        return _validate_list(value, validator)
+    elif isinstance(validator, tuple):
+        return _validate_tuple(value, validator)
     elif callable(validator):
         return validator(value)
     else:
@@ -30,10 +50,22 @@ def _validate_dict(value, validator):
             problems[attr_name] = result
     return problems or None
 
-def _validate_sequence(value, validator):
+def _validate_list(value, validator):
     problems = {}
+    if len(validator) != 1:
+        raise ValueError('got multiple element validators for list')
     element_validator = validator[0]
     for index, element in enumerate(value):
+        result = element_validator(element)
+        if result:
+            problems[index] = result
+    return problems or None
+
+def _validate_tuple(value, validator):
+    problems = {}
+    if len(value) != len(validator):
+        problems['__all__'] = 'unexpected tuple length: expected {0}, got {1}'.format(len(validator), len(value))
+    for index, (element, element_validator) in enumerate(zip(value, validator)):
         result = element_validator(element)
         if result:
             problems[index] = result
